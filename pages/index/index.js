2ccfd6197f730ee1061976db30b08e778d1dd050
huang-menghang <449043209@qq.com>
2018-03-21 15:33:59 +0800

    添加首页分页添加

--- pages/index/index.js
+++ pages/index/index.js
@@ -15,6 +15,8 @@ Page({
       selectCurrent: 0,
       categories: [],
       activeCategoryId: 0,
+      page:1,
+      pageSize:4,
       goods: [],
       scrollTop: "0",
       loadingMoreHidden: true,
@@ -47,7 +49,7 @@ Page({
         }
       })
       wx.request({
-        url: app.globalData.serverPath +"/wxapplet/commodity/category",
+        url: app.globalData.serverPath +"/wxapplet/category",
         success: function (res) {
           var categories = [{ id: 0, categoryName: "全部" }];
           console.log(res);
@@ -58,9 +60,12 @@ Page({
           }
           that.setData({
             categories: categories,
-            activeCategoryId: 0
+            activeCategoryId: 0,
+            page:1,
+            gooods:[],
+            loadingMoreHidden: true
           });
-          //that.getGoodsList(0);
+          that.getGoodsList(0);
         }
       })
       that.getCoupons();
@@ -92,9 +97,12 @@ Page({
     },
     tabClick: function (e) {
       this.setData({
-        activeCategoryId: e.currentTarget.id
+        activeCategoryId: e.currentTarget.id,
+        page:1,
+        goods:[],
+        loadingMoreHidden: true
       });
-      //this.getGoodsList(this.data.activeCategoryId);
+      this.getGoodsList(this.data.activeCategoryId);
     },
     getNotice: function () {
       var that = this;
@@ -110,6 +118,11 @@ Page({
             });
           }
         }
+      })
+    },
+    toDetailsTap: function (e) {
+      wx.navigateTo({
+        url: '/pages/commodity-info/index?commodityId=' + e.currentTarget.dataset.id
       })
     },
     getCoupons: function () {
@@ -130,8 +143,66 @@ Page({
       //   }
       // })
     },
+    getGoodsList: function (categoryId){
+      var that = this;
+      console.log("页数:  "+this.data.page);
+      wx.request({
+        url: app.globalData.serverPath + "/wxapplet/category/" + categoryId +"/commodity",
+        data:{
+          nameLike: that.data.searchInput,
+          pageNum:that.data.page,
+          pageSize:that.data.pageSize
+        },
+        success: function (res) {
+          console.log(res);
+          var goodslistTem = that.data.goods;
+          if (that.data.pageNum == 1){
+            goodslistTem = []
+          }
+          var goodslist = res.data.items;
+          if (res.data.lastPage) {
+            that.setData({
+              goods: goodslistTem.concat(goodslist),
+              loadingMoreHidden: false
+            })
+          } else {
+            that.setData({
+              goods: goodslistTem.concat(goodslist),
+              loadingMoreHidden: true,
+              page: that.data.page + 1
+            })
+          }
+        }
+      })
+    },
+    listenerSearchInput: function (e) {
+      this.setData({
+        searchInput: e.detail.value
+      })
 
-
+    },
+    toSearch: function () {
+      this.setData({
+        page:1,
+        goods:[],
+        loadingMoreHidden:true
+      })
+      this.getGoodsList(this.data.activeCategoryId);
+    },
+    /**
+      * 页面相关事件处理函数--监听用户下拉动作
+      */
+    onPullDownRefresh: function () {
+      console.log("下拉");
+    },
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+      if (this.data.loadingMoreHidden){
+          this.getGoodsList(this.data.activeCategoryId);
+      }
+    }
 
 
 })
